55d1d0dff5ccc8b3cd27bd81cd4288e6139bcfb7
Andrew D. Pease <7442091+peasead@users.noreply.github.com>
2019-04-19 11:58:03 -0500

    fixed issue with restarting gitea after config

--- deploy_capes.sh
+++ deploy_capes.sh
@@ -94,7 +94,7 @@ sudo docker run -d  --network capes --restart unless-stopped --name capes-landin
 sudo docker run -d --network capes --restart unless-stopped --name capes-cyberchef -p 8000:8080 remnux/cyberchef:latest
 
 # Gitea Service
-sudo docker run -d --network capes --restart unless-stopped --name capes-gitea -v /var/lib/docker/volumes/gitea/_data:/data:z -e "VIRTUAL_PORT=3000" -e "VIRTUAL_HOST=capes-gitea" -p 2222:22 -p 4000:3000 gitea/gitea:latest
+sudo docker run -d --network capes --restart unless-stopped --name capes-gitea -v /var/lib/docker/volumes/gitea/_data:/data:z -e "VIRTUAL_PORT=4000" -e "VIRTUAL_HOST=capes-gitea" -p 2222:22 -p 4000:4000 gitea/gitea:latest
 
 # Etherpad Service
 sudo docker run -d --network capes --restart unless-stopped --name capes-etherpad -e "ETHERPAD_TITLE=CAPES" -e "ETHERPAD_PORT=9001" -e ETHERPAD_ADMIN_PASSWORD=$etherpad_admin_passphrase -e "ETHERPAD_ADMIN_USER=admin" -e "ETHERPAD_DB_TYPE=mysql" -e "ETHERPAD_DB_HOST=capes-etherpad-mysql" -e "ETHERPAD_DB_USER=etherpad" -e ETHERPAD_DB_PASSWORD=$etherpad_mysql_passphrase -e "ETHERPAD_DB_NAME=etherpad" -p 5000:9001 tvelocity/etherpad-lite:latest
