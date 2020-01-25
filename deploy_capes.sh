49d72bb7db837856d200f41d4214c9987f4c0e3f
Andrew D. Pease <7442091+peasead@users.noreply.github.com>
2020-01-24 22:23:43 -0600

    thehive and cortex behind ssl

--- deploy_capes.sh
+++ deploy_capes.sh
@@ -110,13 +110,13 @@ docker run -d --network capes --restart unless-stopped --name capes-etherpad -e 
 # Integrating Cortex with TheHive, read below
 # https://github.com/TheHive-Project/CortexDocs/blob/master/admin/quick-start.md#step-7-optional-create-an-account-for-thehive-integration
 # https://github.com/TheHive-Project/TheHiveDocs/blob/master/admin/configuration.md#6-cortex
-docker run -d --network capes --restart unless-stopped --name capes-thehive -p 9000:9000 -v $(pwd)/application.conf:/etc/thehive/application.conf:z thehiveproject/thehive:3.4.0 --es-hostname capes-thehive-elasticsearch
+docker run -d --network capes --restart unless-stopped --name capes-thehive -v $(pwd)/application.conf:/etc/thehive/application.conf:z thehiveproject/thehive:3.4.0 --es-hostname capes-thehive-elasticsearch
 
 # Cortex Service
 # Integrating Cortex with TheHive, read below
 # https://github.com/TheHive-Project/CortexDocs/blob/master/admin/quick-start.md#step-7-optional-create-an-account-for-thehive-integration
 # https://github.com/TheHive-Project/TheHiveDocs/blob/master/admin/configuration.md#6-cortex
-docker run -d --network capes --restart unless-stopped --name capes-cortex -p 9001:9001 thehiveproject/cortex:3.0.1 --es-hostname capes-thehive-elasticsearch
+docker run -d --network capes --restart unless-stopped --name capes-cortex thehiveproject/cortex:3.0.1 --es-hostname capes-thehive-elasticsearch
 
 # Draw.io Service
 docker run -d --network capes --restart unless-stopped --name capes-draw.io fjudith/draw.io
